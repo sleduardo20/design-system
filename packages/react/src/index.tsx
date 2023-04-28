e34bca64a5c57c9a2146cf167e9a1f8ffe8fb337
sleduardo20 <sl.eduardo20@gmail.com>
2023-04-28 19:58:58 -0300

    🛠 config storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,29 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.gray900 }}>OK ok</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  fontWeight: '$bold',
+  backgroundColor: '$gray100',
+  borderRadius: '$sm',
+  border: 'none',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
