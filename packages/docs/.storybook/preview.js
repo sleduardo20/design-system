ec05b6aa295bae793f6b81c9a29d0a96ab4ea975
sleduardo20 <sl.eduardo20@gmail.com>
2023-04-28 19:59:16 -0300

    🛠 config storybook

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,22 @@
+import {themes} from '@storybook/theming'
+/** @type { import('@storybook/react').Preview } */
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default:'dark'
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      theme:themes.dark
+    }
+  },
+};
+
+export default preview;
