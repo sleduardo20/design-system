35d9554ad93f872552fc8ce450bf7bc7b5a85054
sleduardo20 <sl.eduardo20@gmail.com>
2023-05-01 16:12:53 -0300

    ✨ create TextInput component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -15,6 +15,7 @@ export default {
       </>
     ),
   },
+
   component: Box,
 } as Meta<BoxProps>
 
---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextInput',
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$1',
+          }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+
+  component: TextInput,
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name.',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'call.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
