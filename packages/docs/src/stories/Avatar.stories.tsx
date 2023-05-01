30fde4d882a671a7a34fbb448165ba11bddb4114
sleduardo20 <sl.eduardo20@gmail.com>
2023-05-01 15:23:14 -0300

    ✨ create Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/56615577?v=4',
+    alt: 'Eduardo Lima',
+  },
+  component: Avatar,
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
